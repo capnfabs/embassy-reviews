6fdf58b2f669808395d147ae0d5c71815a21311a
Fabian Tamp <fabian.tamp@gmail.com>
2016-06-09 23:35:07 +1000

    fetch_review --> fetch_reviews

--- run_everything.sh
+++ run_everything.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-go install ./fetch_embassies ./fetch_review ./process_cities
+go install ./fetch_embassies ./fetch_reviews ./process_cities
 process_cities > out/cities.txt
 PLACES_API_KEY=`cat places_api_key.txt` fetch_embassies <out/cities.txt >out/embassies.txt
 cat out/embassies.txt | sort | uniq >out/embassies_unique.txt
 # Use shuf on linux, gshuf on osx
 gshuf out/embassies_unique.txt >out/embassies_unique_shuffled.txt
 # Don't worry, we also print reviews to Stdout.
- cat out/embassies_unique_shuffled.txt | PLACES_API_KEY=`cat places_api_key.txt` fetch_review --all >out/reviews.txt
+ cat out/embassies_unique_shuffled.txt | PLACES_API_KEY=`cat places_api_key.txt` fetch_reviews --all >out/reviews.txt
 
 # If you want to manually curate, install 'jq' and then
 jq . <out/reviews.json >out/reviews_formatted.json
